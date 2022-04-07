ff42ba7bfcc999537b6d9aff550e6c35507013b2
killu4kun <mateeus.soarees@outlook.com>
2022-04-07 18:53:03 -0300

    imp: category and category page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import Explore from './pages/Explore';
+import Category from './pages/Category';
 import PrivateRoute from './components/PrivateRoute';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
@@ -18,6 +19,7 @@ function App() {
           <Route path='/' element={<Explore />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:category' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
