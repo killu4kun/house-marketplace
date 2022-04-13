10b9b895a4b44a851da7513df46ff0fae9abbfc5
killu4kun <mateeus.soarees@outlook.com>
2022-04-13 18:35:55 -0300

    add map to single listing

--- src/pages/SingleListing.jsx
+++ src/pages/SingleListing.jsx
@@ -5,6 +5,7 @@ import { getAuth } from 'firebase/auth';
 import { db } from '../firebase-config';
 import Spinner from '../components/Spinner';
 import shareIcon from '../assets/svg/shareIcon.svg';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 function SingleListing() {
   const [listing, setListing] = useState(null);
@@ -14,6 +15,11 @@ function SingleListing() {
   const navigate = useNavigate();
   const params = useParams();
   const auth = getAuth();
+
+  const leafletConfig = {
+    height: '100%',
+    width: '100%',
+  };
 
   useEffect(() => {
     const getListing = async () => {
@@ -81,6 +87,24 @@ function SingleListing() {
           <li>{listing.furnished && 'Furnished'}</li>
         </ul>
         <p className='listingLocationTitle'>Location</p>
+        <div className='leafletContainer'>
+          <MapContainer
+            style={leafletConfig}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+              url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
             to={`/contact/${listing.userRef}?listingName=${listing.name}`}
